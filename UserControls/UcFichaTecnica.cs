fe1586edec5657012c271a79943ab42e02d11012
CHRISTIAN JOSUE ROMERO LEON <cromero@tisoluciones.net.pe>
2023-03-28 11:09:26 -0500

    Agregar archivos de proyecto.

---
+++ UserControls/UcFichaTecnica.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SolverMaintApp.Frontend.UserControls
+{
+    public partial class UcFichaTecnica : UserControl
+    {
+        public UcFichaTecnica()
+        {
+            InitializeComponent();
+        }
+    }
+}
